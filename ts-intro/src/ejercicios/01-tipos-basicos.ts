a8ea64c92a9ad19da0e95f6828b9cf4aaf3fff84
Carmen Molinero <carmen_mp3@hotmail.es>
2022-11-20 20:20:07 +0100

    ANGULAR-DE-CERO-A-EXPERTO: Typescript - tipos básicos

---
+++ ts-intro/src/ejercicios/01-tipos-basicos.ts
@@ -0,0 +1,14 @@
+/*
+    ===== Código de TypeScript =====
+*/
+const nombreConst = 'Strider';
+let nombre: string = 'Carmen';
+
+nombre = 'Fernando';
+
+let hp: number | string = 95; //hp es de tipo number o string 
+hp = 'FULL';
+
+let estaVivo: boolean = true;
+
+console.log( nombre, hp );
